eff9fefbf01b3a51e47f3f736a63138fe522b9ba
salozhor <salozhor@gmail.com>
2019-12-08 22:20:17 +0300

    Authentication implemented

--- sign_in/urls.py
+++ sign_in/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from sign_in import views
 
 urlpatterns = [
-    path('', views.sign_in),
-    path('success/', views.success)
+    path('', views.sign_in, name='sign-in'),
+    path('success/', views.success, name='sign-in-success')
 ]
