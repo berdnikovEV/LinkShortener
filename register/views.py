96b6d598d031eb8777bb1653931233e735af0550
salozhor <salozhor@gmail.com>
2019-12-12 17:59:00 +0300

    Registration tests added

--- register/views.py
+++ register/views.py
@@ -8,25 +8,25 @@ from django.contrib import messages
 def register(request):
     if request.method == "POST":
 
-        current_user_login_list = (user.username for user in User.objects.all())
+        current_user_username_list = (user.username for user in User.objects.all())
         current_user_email_list = (user.email for user in User.objects.all())
         
-        login = request.POST['login'] 
+        username = request.POST['username'] 
         email = request.POST['email']
         password = request.POST['password']
         password_confirmation = request.POST['password_confirmation']
         
-        if '' in (login, email, password):
+        if '' in (username, email, password):
             messages.add_message(request, messages.WARNING, "All fields must be filled")
-            return render(request, 'register_main.html', {'login':login, 'email':email})
+            return render(request, 'register_main.html', {'username':username, 'email':email})
 
 
         if password !=  password_confirmation:
             messages.add_message(request, messages.WARNING, "Password didn't match its confirmation")
-            return render(request, 'register_main.html', {'login':login, 'email':email})
+            return render(request, 'register_main.html', {'username':username, 'email':email})
 
-        if email not in current_user_email_list and login not in current_user_login_list:
-            user = User.objects.create_user(login, email=email, password=password)
+        if email not in current_user_email_list and username not in current_user_username_list:
+            user = User.objects.create_user(username, email=email, password=password)
             user.save()
             return redirect("register-success")
         else:
