a53054a4f2b7359f6da81f8dece9818b00a5c914
salozhor <salozhor@gmail.com>
2019-12-16 22:44:05 +0300

    shortened_link was renamed to short_code (better description)

--- shorten/models.py
+++ shorten/models.py
@@ -4,16 +4,16 @@ from .utils import get_new_code
 
 class ShortenedLink(models.Model):
     initial_url = models.CharField(max_length=100)
-    shortened_url = models.CharField(max_length=5, unique=True)
+    short_code = models.CharField(max_length=6, unique=True)
 
     tags = models.CharField(max_length=100)
     owner_username = models.CharField(max_length=32)
 
     def __str__(self):
-        return str(self.shortened_url)
+        return str(self.short_code)
 
     def save(self, *args, **kwargs):
-        self.shortened_url = get_new_code(self) \
-                            if not self.shortened_url \
-                            else self.shortened_url
+        self.short_code = get_new_code(self) \
+                            if not self.short_code \
+                            else self.short_code
         super(ShortenedLink, self).save(*args, **kwargs)
--- shorten/utils.py
+++ shorten/utils.py
@@ -5,7 +5,7 @@ def get_new_code(obj):
     code = rstr.xeger(r'[a-zA-Z0-9]{6}')
     ShortenedLink = obj.__class__
 
-    if ShortenedLink.objects.filter(shortened_url=code).exists():
+    if ShortenedLink.objects.filter(short_code=code).exists():
         return get_new_code(obj)
 
     return code
