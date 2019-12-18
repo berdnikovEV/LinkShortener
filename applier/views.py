1d13aab69962cabae074a3da2ff602338313b5b4
salozhor <salozhor@gmail.com>
2019-12-18 03:58:01 +0300

    usage stats are now being recorded

--- applier/views.py
+++ applier/views.py
@@ -5,11 +5,16 @@ from shorten.models import ShortenedLink
 def applier(request, short_code=''):
     link = ShortenedLink.objects.filter(short_code=short_code)
     if link.exists():
+        obj = link[0]
+
         context = {
-            'is_valid': "Valid",
-            'destination': link[0].initial_url,
-            'tag_list': link[0].tags
+            'is_valid': 'Valid',
+            'destination': obj.initial_url,
+            'tag_list': obj.tags
         }
+        obj.usage_count += 1
+        obj.save()
+        link[0].save()
         return render(request, 'applier.html', context)
     else:
         context = {
